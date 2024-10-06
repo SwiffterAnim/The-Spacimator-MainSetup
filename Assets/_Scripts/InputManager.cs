8fef9c2be3f1334bb0779366ecec4b04e4a28a4a
SwiffterAnim <rodriccosta@gmail.com>
2024-10-06 15:53:54 +0200

    Testing new Event System
    Did a simple test with the double click functionality

--- Assets/_Scripts/InputManager.cs
+++ Assets/_Scripts/InputManager.cs
@@ -6,6 +6,59 @@ public class InputManager : MonoBehaviour
 {
     private Vector2 mousePosition;
     private RaycastHit2D[] mouseHitAllArray;
+    private UserInputActions userInputActions;
+
+    private bool firstOfTwoClicked = false;
+    private bool secondOfTwoClicked = false;
+    private float timer;
+    private float timeLimitToActivateDoubleClick = 0.45f;
+
+    [Header("Events")]
+    public GameEvent onDoubleClickPerformed;
+
+    private void Start()
+    {
+        userInputActions = GameManager.Instance.GetInputAction();
+        userInputActions.EditingCurve.MoveMarker.performed += MoveMarker_performed;
+    }
+
+    private void OnDisable()
+    {
+        userInputActions.EditingCurve.MoveMarker.performed -= MoveMarker_performed;
+    }
+
+    private void Update()
+    {
+        //Checking for Double Click.
+        if (firstOfTwoClicked)
+        {
+            timer += Time.unscaledDeltaTime;
+            if (timer < timeLimitToActivateDoubleClick && secondOfTwoClicked)
+            {
+                onDoubleClickPerformed.Raise(this, secondOfTwoClicked);
+                firstOfTwoClicked = false;
+                secondOfTwoClicked = false;
+                timer = 0f;
+            }
+            else if (timer >= timeLimitToActivateDoubleClick && !secondOfTwoClicked)
+            {
+                firstOfTwoClicked = false;
+                timer = 0f;
+            }
+        }
+    }
+
+    private void MoveMarker_performed(InputAction.CallbackContext context)
+    {
+        if (!firstOfTwoClicked)
+        {
+            firstOfTwoClicked = true;
+        }
+        else
+        {
+            secondOfTwoClicked = true;
+        }
+    }
 
     //Not sure if this isn't returning the same as Input.mousePosition lol
     public Vector2 GetWorldMouseLocation2D()
---
+++ Assets/_Scripts/TestResponder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestResponder : MonoBehaviour
+{
+    public void ResponseActivated()
+    {
+        Debug.Log("I'm listening to that event!");
+    }
+}
